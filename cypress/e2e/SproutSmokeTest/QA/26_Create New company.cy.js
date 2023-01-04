a7afa1bd94c7011ce903bce8f20bc355b1de111d
MikeZ2022 <mike.z@getsprout.co>
2023-01-04 16:21:34 +1300

    update create new company case

---
+++ cypress/e2e/SproutSmokeTest/QA/26_Create New company.cy.js
@@ -0,0 +1,41 @@
+///<reference types="cypress"/>
+
+describe('SproutSmokeTest_QA',()=>{
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.viewport('macbook-13')
+        cy.loginWithLocalstorage()
+        cy.visit('https://qa.getsprout.co/')
+      })
+    
+    it('1.26 Create new company',()=>{
+        cy.contains('Sign up').click()
+        cy.url().should('contains','auth/signup')
+        cy.get('#user_name').type('TestFromMike')
+        cy.get('#email').type('TestFromMike')
+        cy.get('#password').type('mike1983')
+        cy.get('#confirmPassword').type('Mike1983')
+        cy.contains('Email is invalid').should('be.exist')
+        cy.contains('The two passwords that you entered do not match').should('be.exist')
+        cy.get(':nth-child(5) > .ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-input-affix-wrapper > .ant-input-suffix > .anticon > svg').click()
+        cy.get('#title').type('Mike_test') 
+        cy.get('#company_name').type('TestCompany')
+        cy.get('#company_size').type('TestFromMike')
+        cy.get('#comment')
+          .type('TestFromMike')
+        cy.get('.ant-form-item-control-input-content > .ant-btn').click()
+        cy.get(':nth-child(4) > a').click()
+        cy.get('#email').type('mikez.test009@gmail.com')
+        cy.get('#password').type('Mike_1983')
+        cy.get('#title').type('Head of head')
+        var timestamp = (new Date()).valueOf();
+        cy.get('#company_name').type('MikeTest'+timestamp)
+        cy.get('#company_size').type('11')
+        cy.get('.ant-btn').click()
+        cy.contains('Plans and Pricing')
+   
+      })
+})
